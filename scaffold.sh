2bb6ea7af0ff560b6f7d859e168cf76f0f96b468
jeremyfelt <jeremy.felt@gmail.com>
2025-03-13 12:37:29 -0700

    Adjust scaffolding script

--- scaffold.sh
+++ scaffold.sh
@@ -46,6 +46,7 @@ sed -i '' "s/\"name\": \"scaffold\"/\"name\": \"$project_name_hyphenated\"/g" pa
 sed -i '' "s|https://github.com/happyprime/scaffold#readme|$project_website|g" package.json
 sed -i '' "s|git+https://github.com/happyprime/scaffold.git|$repository_url|g" package.json
 sed -i '' "s|https://github.com/happyprime/scaffold/issues|$repository_url/issues|g" package.json
+sed -i '' "s/\"description\": \".*\"/\"description\": \"The $project_website website.\"/g" package.json
 
 # Update composer.json
 sed -i '' "s/\"name\": \"happyprime\/scaffold\"/\"name\": \"happyprime\/$project_name_hyphenated\"/g" composer.json
@@ -57,4 +58,21 @@ sed -i '' "s!/mu-plugins/scaffold!/mu-plugins/$project_name_hyphenated!" .gitign
 # Update .deploy_include
 sed -i '' "s!/themes/scaffold!/themes/$project_name_hyphenated!" .deploy_include
 
+# Replace PHP constants
+find . -type f -name "*.php" -exec sed -i '' "s/SCAFFOLD_MU_PLUGIN_DIR/${project_name_underscored}_MU_PLUGIN_DIR/g" {} +
+find . -type f -name "*.php" -exec sed -i '' "s/SCAFFOLD_MU_PLUGIN_FILE/${project_name_underscored}_MU_PLUGIN_FILE/g" {} +
+
+# Replace text domain strings (with single quotes)
+find . -type f -name "*.php" -exec sed -i '' "s/'scaffold'/'$project_name_hyphenated'/g" {} +
+
+# Replace text domain in theme template files
+find themes/"$project_name_hyphenated" -type f -name "*.php" -exec sed -i '' "s/scaffold/$project_name_hyphenated/g" {} +
+
+# Update package.json script paths
+sed -i '' "s/mu-plugins\/scaffold/mu-plugins\/$project_name_hyphenated/g" package.json
+sed -i '' "s/themes\/scaffold/themes\/$project_name_hyphenated/g" package.json
+
 echo "Scaffolding complete! Project has been renamed to $project_name_hyphenated"
+
+echo -e "\nChecking for remaining instances of 'scaffold' (case insensitive):"
+find . -type f -not -path "*/\.*" -not -path "*/node_modules/*" -not -path "*/vendor/*" -exec grep -l -i "scaffold" {} \;
